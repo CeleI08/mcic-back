710ccf3c6acd5d83fa8554f3c15423c5ce7c160c
CeleI08 <96275129+CeleI08@users.noreply.github.com>
2022-07-31 22:23:28 -0300

    creacion back

---
+++ src/main/java/com/portfolio/mcic/McicApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.mcic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McicApplication.class, args);
+    }
+ 
+}
---
+++ src/test/java/com/portfolio/mcic/McicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.mcic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
