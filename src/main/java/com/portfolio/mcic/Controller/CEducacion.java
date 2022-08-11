35a6fda8d52000a537fa658c5619a72ddfd98d1b
CeleI08 <96275129+CeleI08@users.noreply.github.com>
2022-08-11 02:28:07 -0300

    corrigiendo cors

--- src/main/java/com/portfolio/mcic/Controller/CEducacion.java
+++ src/main/java/com/portfolio/mcic/Controller/CEducacion.java
@@ -24,8 +24,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("educacion")
-@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/educacion")
+@CrossOrigin(origins = "https://mcicfront.web.app")
 public class CEducacion {
 
     @Autowired
